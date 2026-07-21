d96c2e54aa6c9a4b4b3367659f13e422e87f8f78
Steven <sheigl@gmail.com>
2026-07-21 18:27:01 -0400

    remove api key

--- training_data/generate_synthetic_data/run_game_theory.sh
+++ training_data/generate_synthetic_data/run_game_theory.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 NUM=${1:-1}
-MODEL=${2:-https://server.tailc63ae8.ts.net:4444/v1,openai,gemma4:12b,sk-UA85o9nOaSFX6IA0xjyK3kvjLRcZcsPm}
-VALIDATION_MODEL=${3:-https://server.tailc63ae8.ts.net:4444/v1,openai,qwen3.6:27b,sk-UA85o9nOaSFX6IA0xjyK3kvjLRcZcsPm}
+MODEL=${2:-https://server.tailc63ae8.ts.net:4444/v1,openai,gemma4:12b,$LITELLM_API_KEY}
+VALIDATION_MODEL=${3:-https://server.tailc63ae8.ts.net:4444/v1,openai,qwen3.6:27b,$LITELLM_API_KEY}
 VALIDATION_PCT=${4:-1}
 DRY_RUN=${5:-}
 
